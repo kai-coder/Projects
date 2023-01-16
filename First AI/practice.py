2b56e11d3b7f0e3899232ef513ceeabe9b9d7cdb
kai-coder <54784121+kai-coder@users.noreply.github.com>
2023-01-15 21:48:59 -0500

    Add files via upload

--- First AI/practice.py
+++ First AI/practice.py
@@ -37,6 +37,32 @@ def printLayerCalculations(inputs, weights, biases):
     print("]")
     return output
 
+def printLayerCalculations2(inputs, weights):
+    print("[", end="")
+    for index in range(len(weights.T)):
+        print("(", end="")
+        for index2 in range(len(inputs)):
+            print(f"x{index2}*w{index2}{index}", end="")
+            if index2 == len(inputs) - 1:
+                print(")", end="")
+                print(f" + b{index}", end="")
+                if index != len(weights.T) - 1:
+                    print(", ", end="")
+            else:
+                print(" + ", end="")
+    print("]")
+    print("[", end="")
+    for index in range(len(weights.T)):
+        print(f"Σi(xi*wi{index})", end="")
+        if index2 == len(inputs) - 1:
+            print(f" + b{index}", end="")
+            if index != len(weights.T) - 1:
+                print(", ", end="")
+        else:
+            print(" + ", end="")
+    print("]")
+    print(f"∀j(Σi(xi*wij) + bj)")
+
 def ReLUCalculations(input):
     print("[", end="")
     for i, index in zip(input, range(len(input))):
@@ -45,6 +71,9 @@ def ReLUCalculations(input):
             print(", ", end="")
     print("]")
     return np.maximum(0, input)
+
+def ReLUCalculations2():
+    print(f"∀j(max(0, Σi(xi*wij) + bj))")
 
 def softMaxCalculations(input):
     print("[", end="")
@@ -59,7 +88,23 @@ def softMaxCalculations(input):
         if index != len(input) - 1:
             print(", ", end="")
     print("]")
-    return np.maximum(0, input)
+    expVal = np.exp(input)
+    return expVal / np.sum(expVal)
+
+def lossCalculation(input, expectedValue):
+    for i, v, index in zip(input, expectedValue, range(len(input))):
+        print(f"{v} * log({i})", end="")
+        if index != len(input):
+            print(f" + ", end="")
+    return -np.dot(np.log(input), expectedValue)
+
+def softMaxCalculations2():
+    print("Dividend = exponentiate e to the neuron value:")
+    print("∀j(e^(Σi(xi*wij) + bj))")
+    print("Divisor = sum of all exponentiated neuron values(instead of ∀ it has Σ):")
+    print("Σk(e^(Σi(xi*wik) + bk))")
+    print()
+    print("∀j(e^(Σi(xi*wij) + bj) / Σk(e^(Σi(xi*wik) + bk)))")
 
 def printLayerOutputs(output):
     print("[", end="")
@@ -69,11 +114,11 @@ def printLayerOutputs(output):
             print(", ", end="")
     print("]")
 
-dataSize = 2
+dataSize = 3
 testInput = np.random.randint(-5, 5, (dataSize))
 
-neuronNum = 3
-neuronNum2 = 3
+neuronNum = 4
+neuronNum2 = 4
 neuronNum3 = 2
 testWeight1 = np.random.randint(-5, 5, (neuronNum, dataSize)).T
 testBias1 = np.random.randint(-5, 5, (neuronNum))
@@ -82,6 +127,10 @@ testBias2 = np.random.randint(-5, 5, (neuronNum2))
 testWeight3 = np.random.randint(-5, 5, (neuronNum3, neuronNum2)).T
 testBias3 = np.random.randint(-5, 5, (neuronNum3))
 output = np.dot(testInput, testWeight1) + testBias1
+expectedOutput = np.eye(neuronNum3)[np.random.randint(0, neuronNum3, (1))[0]]
+print("Expected Output:")
+print(expectedOutput)
+print()
 print(f"Input Layer ({dataSize} neurons):")
 print(testInput)
 print()
@@ -97,221 +146,96 @@ print(f"Output Layer ({neuronNum3} neurons):")
 print(testWeight3)
 print(testBias3)
 print()
-print("First Neural Layer Forward Propagation:")
+print()
+print("FIRST NEURAL LAYER:")
+print("Forward Propagation:")
 output = printLayerCalculations(testInput, testWeight1, testBias1)
 print()
-print("First Neural Layer Activation Function:")
+print("Activation Function:")
 output = ReLUCalculations(output)
-print("∀j(")
-print("   max(")
-print("       0,")
-print("       Σi(")
-print("          xi*w1ij")
-print("   )")
-print("   + b1k")
-print(")")
 print()
-print("First Neural Layer Output:")
+print("Output:")
 printLayerOutputs(output)
 print()
-print("Second Neural Layer Forward Propagation:")
+print("Forward Propagation Equation:")
+printLayerCalculations2(testInput, testWeight1)
+print()
+print("Forward Propagation + Activation Equation:")
+ReLUCalculations2()
+print()
+print()
+print("SECOND NEURAL LAYER:")
+print("Forward Propagation:")
 output2 = printLayerCalculations(output, testWeight2, testBias2)
 print()
-print("Second Neural Layer Activation Function:")
+print("Activation Function:")
 output2 = ReLUCalculations(output2)
-print("∀j(")
-print("   max(")
-print("       0,")
-print("       Σi(")
-print("          ∀j(")
-print("             max(")
-print("                 0,")
-print("                 Σi(")
-print("                    xi*w1ij")
-print("                 )")
-print("                 + b1j")
-print("             )")
-print("          )i*w2ij")
-print("       )")
-print("       + b2j")
-print("   )")
-print(")")
-print()
-print("Second Neural Layer Output:")
+print("Output:")
 printLayerOutputs(output2)
 print()
-print("Output Layer Forward Propagation:")
+print("x = last layer output: ", end="")
+ReLUCalculations2()
+print("Forward Propagation Equation:")
+printLayerCalculations2(testInput, testWeight1)
+print()
+print("Forward Propagation + Activation Equation:")
+ReLUCalculations2()
+print()
+print("Full equation:")
+print("∀j(max(0, Σi(∀j(max(0, Σi(xi*wij) + bj))i*wij) + bj))")
+print()
+print()
+print("OUTPUT LAYER:")
+print("Forward Propagation:")
 output3 = printLayerCalculations(output2, testWeight3, testBias3)
 print()
-print("Output Layer Activation Function:")
+print("Activation Function:")
 output3 = softMaxCalculations(output3)
-print("∀j(")
-print("   e^(")
-print("      Σi(")
-print("         ∀j(")
-print("            max(")
-print("                0,")
-print("                Σi(")
-print("                   ∀j(")
-print("                      max(")
-print("                          0,")
-print("                          Σi(")
-print("                             xi*w1ij")
-print("                          )")
-print("                          + b1j")
-print("                      )")
-print("                   )i*w2ij")
-print("                )")
-print("                + b2j")
-print("             )")
-print("         )")
-print("      )i*w3ij + b3j")
-print("   )")
-print("   /")
-print("   Σk(")
-print("      e^(")
-print("         Σi(")
-print("            ∀j(")
-print("               max(")
-print("                   0,")
-print("                   Σi(")
-print("                      ∀j(")
-print("                         max(")
-print("                             0,")
-print("                             Σi(")
-print("                                xi*w1ij")
-print("                             )")
-print("                             + b1j")
-print("                         )")
-print("                      )i*w2ij")
-print("                   )")
-print("                   + b2j")
-print("                )")
-print("            )")
-print("         )i*w3ik + b3k")
-print("      )")
-print("   )")
-print(")")
 print()
-print("Output Layer Output:")
+print("Output:")
 printLayerOutputs(output3)
 print()
-print("[", end="")
-for index in range(len(testWeight1.T)):
-    print("(", end="")
-    for index2 in range(len(testInput)):
-        print(f"x{index2}*w{index2}{index}", end="")
-        if index2 == len(testInput) - 1:
-            print(")", end="")
-            print(f" + b{index}", end="")
-            if index != len(testWeight1.T) - 1:
-                print(", ", end="")
-        else:
-            print(" + ", end="")
-print("]")
-print("[", end="")
-for index in range(len(testWeight1.T)):
-    print(f"Σi(xi*wi{index})", end="")
-    if index2 == len(testInput) - 1:
-        print(f" + b{index}", end="")
-        if index != len(testWeight1.T) - 1:
-            print(", ", end="")
-    else:
-        print(" + ", end="")
-print("]")
-print(f"∀j(Σi(xi*wij) + bj)")
+print("x = last layer output: ", end="")
+print("∀j(max(0, Σi(∀j(max(0, Σi(xi*wij) + bj))i*wij) + bj))")
+print("Forward Propagation Equation:")
+printLayerCalculations2(testInput, testWeight1)
 print()
-print(f"f(x, w, b) = Σxw + b")
-print("[", end="")
-for i in range(len(testInput)):
-    if i != 0:
-        print(end=" ")
-    print("[", end="")
-    for w, b in zip(range(len(testWeight1)), range(len(testBias1[0]))):
-        print(f"f(x{i}, w:{w}, b{b})", end="")
-        if w != len(testWeight1) - 1:
-            print(", ", end="")
-        elif i != len(testInput) - 1:
-            print("], ")
-        else:
-            print("]", end="")
-print("]")
+print("Forward Propagation + Activation Equation:")
+softMaxCalculations2()
 print()
-print("df_dxij = df_d(xij*wjk) * d(xij*wjk)_dxij")
-print("df_d(xij*wjk) = 1")
-print("d(xij*wjk)_dxij = wjk")
-print("df_dxij = 1 * wjk")
+print("Full equation:")
+print("∀j(e^(Σi(∀j(max(0, Σi(∀j(max(0, Σi(xi*wij) + bj))i*wij) + bj))i*wij) + bj) / Σk((e^Σi(∀j(max(0, Σi(∀j(max(0, Σi(xi*wij) + bj))i*wij) + bj))i*wik) + bk))))")
 print()
-print("df_dxij = wjk")
-print("[", end="")
-for i in range(len(testInput)):
-    if i != 0:
-        print(end=" ")
-    print("[", end="")
-    for z in range(len(testInput[i])):
-        print(f"df_dx{i}{z}", end="")
-        if z != len(testInput[i]) - 1:
-            print(", ", end="")
-        elif i != len(testInput) - 1:
-            print("], ")
-        else:
-            print("]", end="")
-print("]")
+print("Equation with clearer variable names:")
+print("∀j(e^(Σi(∀j(max(0, Σi(∀j(max(0, Σi(xi*w1ij) + b1j))i*w2ij) + b2j))i*w3ij) + b3j) / Σk((e^Σi(∀j(max(0, Σi(∀j(max(0, Σi(xi*w1ij) + b1j))i*w2ij) + b2j))i*w3ik) + b3k))))")
 print()
-print("[", end="")
-for i in range(len(testInput)):
-    if i != 0:
-        print(end=" ")
-    print("[", end="")
-    for c in range(len(testInput[i])):
-        print("[", end="")
-        for w in range(len(testWeight1)):
-            print(f"w{c}{w}", end="")
-            if w == len(testWeight1) - 1:
-                if c != len(testInput[i]) - 1:
-                    print("], ", end="")
-                elif i != len(testInput) - 1:
-                    print("]], ")
-                else:
-                    print("]", end="")
-            else:
-                print(", ", end="")
-print("]]")
 print()
-print("[", end="")
-for w, b in zip(range(len(testWeight1)), range(len(testBias1[0]))):
-    if w != 0:
-        print(end=" ")
-    print("[", end="")
-    for i in range(len(testInput)):
-        print(f"df_dw{w}{i}", end="")
-        if i != len(testInput) - 1:
-            print(", ", end="")
-        elif w != len(testWeight1) - 1:
-            print("], ")
-        else:
-            print("]", end="")
-print("]")
+print("LOSS:")
+print("Loss Calculation:")
+output4 = lossCalculation(output3, expectedOutput)
 print()
-print("[", end="")
-for i in range(len(testInput)):
-    if i != 0:
-        print(end=" ")
-    print("[", end="")
-    for w in range(len(testWeight1)):
-        for z, c in zip(range(len(testInput[i])), range(len(testWeight1[w]))):
-            print(f"w{c}{w}", end="")
-            if z == len(testInput[i]) - 1:
-                if w != len(testWeight1) - 1:
-                    print(", ", end="")
-                elif i != len(testInput) - 1:
-                    print("], ")
-                else:
-                    print("]", end="")
-            else:
-                print(" + ", end="")
-print("]")
-
+print("Output:")
+print(output4)
+print()
+print("Loss Equation:")
+print("x=∀j(e^(Σi(∀j(max(0, Σi(∀j(max(0, Σi(xi*w1ij) + b1j))i*w2ij) + b2j))i*w3ij) + b3j) / Σk((e^Σi(∀j(max(0, Σi(∀j(max(0, Σi(xi*w1ij) + b1j))i*w2ij) + b2j))i*w3ik) + b3k))))")
+print("-Σl(yl*log(xl))")
+print()
+print("Full equation:")
+print("-Σl(yl*log(∀j(e^(Σi(∀j(max(0, Σi(∀j(max(0, Σi(xi*w1ij) + b1j))i*w2ij) + b2j))i*w3ij) + b3j) / Σk((e^Σi(∀j(max(0, Σi(∀j(max(0, Σi(xi*w1ij) + b1j))i*w2ij) + b2j))i*w3ik) + b3k))))l))")
+print()
+print()
+print("WEIGHT GRADIENTS:")
+print("d(-Σl)_d(w1ij) = d(-Σl)_d(yl*log(xl)) * d(yl*log(xl))_d(log(xl)) * d(log(xl))_d(xl)")
+print("d(-Σl)_d(w1ij) =          -1          *    {yl = 1: 1; yl=0: 0}  *     1 / xl")
+print("d(-Σl)_d(w1ij) = -{yl = 1: 1; yl=0: 0} / xl")
+print("d(-Σl)_d(w1ij) = d(e^(Σi)/Σk(e^(Σi)))_d(Σi)")
+print("d(-Σl)_d(w1ij) = (d(e^(Σi))_d(Σi) * Σk(e^(Σi)) - e^(Σi) * d(Σk(e^(Σi)))_d(Σi)) / (Σk(e^(Σi)))^2")
+print("d(-Σl)_d(w1ij) = (e^(Σi) * Σk(e^(Σi)) - e^(Σi) * d(Σk(e^(Σi)))_d(Σi)) / (Σk(e^(Σi)))^2")
+print("d(Σk(e^(Σi)))_d(Σi) = d(Σk(e^(Σi)))_d(e^(Σi)) * d(e^(Σi))_d(Σi)")
+print("d(Σk(e^(Σi)))_d(Σi) = 1 * e^(Σi)")
+print("d(-Σl)_d(w1ij) = (e^(Σi) * Σk(e^(Σi)) - e^(Σi) * e^(Σi) / (Σk(e^(Σi)))^2")
+print("d(-Σl)_d(w1ij) = -{yl = 1: 1; yl=0: 0} / xl")
 # print()
 # print("Input:", testInput)
 # print("Weight:", testWeight)
